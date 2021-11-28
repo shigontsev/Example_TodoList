47bfc86de37fdc87599c9532bed0114408e5085b
shigontsev <shigontsev1@mail.ru>
2021-11-28 11:23:58 +0400

    add DependencyResolver

---
+++ Dependencies/DependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using BLL;
+using BLL.Interfaces;
+using DAL.Interfaces;
+using DAL.JsonDAL;
+using System;
+
+namespace Dependencies
+{
+    public class DependencyResolver
+    {
+        private static DependencyResolver _instance;
+
+        public static DependencyResolver Instance
+        {
+            get
+            {
+                if (_instance is null)
+                {
+                    _instance = new DependencyResolver();
+                }
+                return _instance;
+            }
+        }
+
+        private ITodoListDAO TodoListDAO => new TodoListDAO();
+
+        public ITodoListLogic TodoListLogic => new TodoListLogic(TodoListDAO);
+
+        private IСompletedTasksDAO СompletedTasksDAO => new СompletedTasksDAO();
+
+        public ICompletedTasksLogic CompletedTasksLogic => new CompletedTasksLogic(СompletedTasksDAO);
+    }
+}
